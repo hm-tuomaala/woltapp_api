8a7f19cb249a95207b01f025a4be9476e4ece836
Henri-Matias Tuomaala <henri-matias.tuomaala@aalto.fi>
2020-02-04 15:50:10 +0200

    Add test and some refactoring

--- endpoint/tests.py
+++ endpoint/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
+from static import parse
+import requests
+import blurhash
 
-# Create your tests here.
+class TestJsonData(TestCase):
+
+    def test_data_attributes(self):
+        try:
+            data = parse.DATA
+        except AttributeError:
+            self.skipTest("Json data not found")
+
+        for item in parse.DATA['restaurants']:
+            # try:
+            #     hash = item["blurhash"]
+            #     city = ["city"]
+            #     currency = item["currency"]
+            #     dp = item["delivery_price"]
+            #     desc = item["description"]
+            #     image = item["image"]
+            #     location = item["location"]
+            #     name = ["name"]
+            #     online = item["online"]
+            #     tags = item["tags"]
+            # except KeyError:
+            self.assertEqual(len(item), 10)
+
+
+    def test_blurhashes(self):
+        try:
+            data = parse.DATA
+        except AttributeError:
+            self.skipTest("Json data not found")
+
+        for item in parse.DATA['restaurants']:
+            # Use x_components=3 and y_components=3
+            if len(item['blurhash']) == 22:
+                img = requests.get(item['image'], stream=True)
+                hash = blurhash.encode(img.raw, x_components=3, y_components=3)
+                self.assertEqual(item['blurhash'], hash)
+            # Use x_components=4 and y_components=4
+            elif len(item['blurhash']) == 36:
+                img = requests.get(item['image'], stream=True)
+                hash = blurhash.encode(img.raw, x_components=4, y_components=4)
+                self.assertEqual(item['blurhash'], hash)
+            # Else test that blurhash lenght is even
+            else:
+                hash_len = len(item['blurhash'])
+                self.assertTrue(hash_len % 2 == 0)
--- static/parse.py
+++ static/parse.py
@@ -14,29 +14,35 @@ def blurhashes_are_equal(calculated, marked_value):
 def validate_blurhashes(data):
     ret = []
     for item in data['restaurants']:
-        calculated = calculate_blurhash(item['image'], 3)
         marked_value = item['blurhash']
 
         # Compare calculated blurhashs to the marked values first with
         # x and y componont values of 3 and if they don't match then component
         # values of 4
-        if not blurhashes_are_equal(calculated, marked_value):
+
+        if len(marked_value == 22):
+            calculated = calculate_blurhash(item['image'], 3)
+        else:
             calculated = calculate_blurhash(item['image'], 4)
-            if not blurhashes_are_equal(calculated, marked_value):
-                ret.append(item)
+
+        if not blurhashes_are_equal(calculated, marked_value):
+            ret.append(item)
+
     return ret
 
 
 
 # Json data is loaded only once to the DATA variable once the server starts
-# and restaurants with invalid blurhashes are stored in INVALID_HASHES
 DATA = {}
-INVALID_HASHES = []
 
 # Load json data from restaurants.json
 with open('static/restaurants.json') as restaurants:
     DATA = json.load(restaurants)
 
+# Restaurants with invalid blurhashes are stored in INVALID_HASHES
+INVALID_HASHES = []
+
 # Validating blurhash values
-# NOTE: Calculating blurhashes is quite expensive
+# NOTE: Calculating blurhashes is quite expensive so searching invalid blurhashes
+# is commented for now:
 # INVALID_HASHES = validate_blurhashes(DATA)
