40a20c898913e47a6e4b9d77a5fbb135e4f7ad6b
Henri-Matias Tuomaala <henri-matias.tuomaala@aalto.fi>
2020-02-04 17:54:03 +0200

    Add more test

--- endpoint/tests.py
+++ endpoint/tests.py
@@ -5,46 +5,56 @@ import blurhash
 
 class TestJsonData(TestCase):
 
-    def test_data_attributes(self):
+    def test_number_of_data_attributes(self):
         try:
             data = parse.DATA
         except AttributeError:
             self.skipTest("Json data not found")
 
-        for item in parse.DATA['restaurants']:
-            # try:
-            #     hash = item["blurhash"]
-            #     city = ["city"]
-            #     currency = item["currency"]
-            #     dp = item["delivery_price"]
-            #     desc = item["description"]
-            #     image = item["image"]
-            #     location = item["location"]
-            #     name = ["name"]
-            #     online = item["online"]
-            #     tags = item["tags"]
-            # except KeyError:
+        for item in data['restaurants']:
             self.assertEqual(len(item), 10)
 
 
-    def test_blurhashes(self):
+    def test_required_attributes(self):
         try:
             data = parse.DATA
         except AttributeError:
             self.skipTest("Json data not found")
 
-        for item in parse.DATA['restaurants']:
-            # Use x_components=3 and y_components=3
-            if len(item['blurhash']) == 22:
-                img = requests.get(item['image'], stream=True)
-                hash = blurhash.encode(img.raw, x_components=3, y_components=3)
-                self.assertEqual(item['blurhash'], hash)
-            # Use x_components=4 and y_components=4
-            elif len(item['blurhash']) == 36:
-                img = requests.get(item['image'], stream=True)
-                hash = blurhash.encode(img.raw, x_components=4, y_components=4)
-                self.assertEqual(item['blurhash'], hash)
-            # Else test that blurhash lenght is even
-            else:
-                hash_len = len(item['blurhash'])
-                self.assertTrue(hash_len % 2 == 0)
+        required_keys = [
+            "blurhash",
+            "city",
+            "currency",
+            "delivery_price",
+            "description",
+            "image",
+            "location",
+            "name",
+            "online",
+            "tags"
+            ]
+
+        for item in data['restaurants']:
+            keys = item.keys()
+            self.assertEqual(required_keys, list(keys))
+
+    def test_blurhash_length_is_even(self):
+        try:
+            data = parse.DATA
+        except AttributeError:
+            self.skipTest("Json data not found")
+
+        for item in data['restaurants']:
+            self.assertTrue(len(item['blurhash']) % 2 == 0)
+            
+
+    def test_blurhashes_x_and_y_components_4(self):
+        try:
+            data = parse.DATA
+        except AttributeError:
+            self.skipTest("Json data not found")
+
+        for item in data['restaurants']:
+            img = requests.get(item['image'], stream=True)
+            hash = blurhash.encode(img.raw, x_components=4, y_components=4)
+            self.assertEqual(item['blurhash'], hash)
